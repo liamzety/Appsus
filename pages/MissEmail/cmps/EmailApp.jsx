e66c9c332ff4b3f1abfc5406562ab8e86647fce1
liamzety <58632095+liamzety@users.noreply.github.com>
2020-08-26 11:44:09 +0300

    d

---
+++ pages/MissEmail/cmps/EmailApp.jsx
@@ -0,0 +1,13 @@
+
+
+export class EmailApp extends React.Component {
+
+    render() {
+        return (
+            <section>
+
+                <h1>EMAIL APP COMP</h1>
+            </section>
+        )
+    }
+}
