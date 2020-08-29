66b5fc7739e386134bd4a21a4019ffb89233ba2d
liamzety <58632095+liamzety@users.noreply.github.com>
2020-08-29 16:56:51 +0300

    home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,24 @@
-export class Home extends React.Component{
+export class Home extends React.Component {
 
-    render(){
-        return(
-            <section className="home">
-                <h1>Home</h1>
-            </section>
+    render() {
+        return (
+            <React.Fragment>
+
+                <section className="home">
+                    <div className="vid-wrapper">
+                        <video autoPlay src="../assets/vid/appsus-vid.mp4"></video>
+                    </div>
+                    <div className="home-links">
+                        <button>GO TO KEEP</button>
+                        <button>GO TO BOOKS</button>
+                        <button>GO TO EMAILS</button>
+                        <button>GO TO ABOUT</button>
+                    </div>
+                </section>
+                <footer>
+                    coffeerights
+            </footer>
+            </React.Fragment>
         )
     }
 }
