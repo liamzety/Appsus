b17fe72a59df8ba97337274ca25bf621361e8cd9
liamzety <58632095+liamzety@users.noreply.github.com>
2020-08-29 17:28:10 +0300

    done

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -9,15 +9,17 @@ export class Home extends React.Component {
                         <video autoPlay src="../assets/vid/appsus-vid.mp4"></video>
                     </div>
                     <div className="home-links">
-                        <button>GO TO KEEP</button>
-                        <button>GO TO BOOKS</button>
-                        <button>GO TO EMAILS</button>
-                        <button>GO TO ABOUT</button>
+                        <div className="home-link"> <i className="fas fa-clipboard"></i></div>
+                        <div className="home-link">    <i className="fas fa-book"></i></div>
+                        <div className="home-link">  <i className="fas fa-mail-bulk"></i></div>
+                        <div className="home-link">
+                            <i className="fas fa-user"></i></div>
+
+
+
                     </div>
                 </section>
-                <footer>
-                    coffeerights
-            </footer>
+
             </React.Fragment>
         )
     }
