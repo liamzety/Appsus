f8b57fb5b967c5e617fa4c5797f28578daa0441c
Your Name <you@example.com>
2020-08-29 16:09:46 +0300

    message

--- pages/About.jsx
+++ pages/About.jsx
@@ -3,18 +3,47 @@ export class About extends React.Component {
     render() {
         return (
             <section className="about">
-                <h1>About</h1>
+                <h1>About The Creators</h1>
 
+                <section className="creators-container">
+
+                
                 <div className="roei-container">
-                <div className="img-container">
-                    <img src="../assets/img/prrofile-pic.jpeg" alt="" />
-                </div>
-                <div className="text-container">
-            <p>Roei is a "Fullstack Developer" </p>
+                    <h3>Roei Arazi</h3>
+                    <div className="info">
+
+                        <div className="img-container">
+                            <img src="../assets/img/prrofile-pic.jpeg" alt="" />
+                        </div>
+                        <div className="text-container">
+                            <p>Roei is a 28 years old who started coding as a hobby and immediatly
+                                fell inlove with the concept and wen straight
+                                to the most intense course he could find he is learning
+                                how to be the best "Fullstack Developer" he can!
+                                as a former trainer Roei believes that you have to put
+                                the work in order to achieve results!</p>
+                                <ul className="social-links">
+                                   <a href="https://www.facebook.com/FriZe6/"><li className="facebook"><i class="fab fa-facebook-square"></i></li></a>
+                                    <a href="https://www.instagram.com/roeifit/"><li className="instagram"><i class="fab fa-instagram-square"></i></li></a>
+                                    <a href="https://www.linkedin.com/in/roei-arazi-b1b42a19a/"><li className="linkedin"><i class="fab fa-linkedin"></i></li></a>
+                                </ul>
+                        </div>
+
+                    </div>
                 </div>
 
-                </div>
+                <div className="liam-container">
+                    <h3>Liam Zety</h3>
+                    <div className="info">
+                        <div className="img-container">
 
+                        </div>
+                        <div className="text-container">
+
+                        </div>
+                    </div>
+                </div>
+                </section>
             </section>
         )
     }
