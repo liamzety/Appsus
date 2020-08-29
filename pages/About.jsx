8b74acee4e437b37c0365c29a5722e00203f0d53
Your Name <you@example.com>
2020-08-29 15:32:43 +0300

    message

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,10 +1,19 @@
-export class About extends React.Component{
+export class About extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <section className="about">
                 <h1>About</h1>
-            
+
+                <div className="roei-container">
+                <div className="img-container">
+                    <img src="../assets/img/prrofile-pic.jpeg" alt="" />
+                </div>
+                <div className="text-container">
+            <p>Roei is a "Fullstack Developer" </p>
+                </div>
+
+                </div>
 
             </section>
         )
