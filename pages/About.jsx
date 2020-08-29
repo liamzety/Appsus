8640cdd3b55a8cfcde4cab46c81d0a60fe51ea08
liamzety <58632095+liamzety@users.noreply.github.com>
2020-08-29 17:48:37 +0300

    k

--- pages/About.jsx
+++ pages/About.jsx
@@ -47,16 +47,11 @@ export class About extends React.Component {
                                     <img src="../assets/img/liam.jpeg" alt="" />
                                 </div>
                                 <div className="text-container">
-                                    <p>Roei is a 28 years old who started coding as a hobby and immediatly
-                                    fell inlove with the concept and wen straight
-                                    to the most intense course he could find he is learning
-                                    how to be the best "Fullstack Developer" he can!
-                                    as a former trainer Roei believes that you have to put
-                                the work in order to achieve results!</p>
+                                    <p>Liam is 26 years old, originaly started Web developing as a self-taught and later went on Coding Academy 3-months intense bootcamp. and ever since, is in over the top relationship with coding. </p>
+
                                     <ul className="social-links">
-                                        <a href="https://www.facebook.com/FriZe6/"><li className="facebook"><i className="fab fa-facebook-square"></i></li></a>
-                                        <a href="https://www.instagram.com/roeifit/"><li className="instagram"><i className="fab fa-instagram"></i></li></a>
-                                        <a href="https://www.linkedin.com/in/roei-arazi-b1b42a19a/"><li className="linkedin"><i className="fab fa-linkedin"></i></li></a>
+                                        <a href="https://www.facebook.com/liamzety"><li className="facebook"><i className="fab fa-facebook-square"></i></li></a>
+                                        <a href="https://www.instagram.com/liam_zety_personal_trainer/"><li className="instagram"><i className="fab fa-instagram"></i></li></a>
                                     </ul>
                                 </div>
                             </div>
